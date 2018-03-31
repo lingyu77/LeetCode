7611d71c178d527e3b0ecf8ef0495672d6112b6b
lingyu77 <34279209+lingyu77@users.noreply.github.com>
2018-03-31 21:30:58 +0100

    234

---
+++ C/234_PalindromeLinkedList.c
@@ -0,0 +1,99 @@
+/*
+234. Palindrome Linked List
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+//8 ms
+bool isPalindrome(struct ListNode* head) {
+    struct ListNode *fast = head, *slow = head;
+    struct ListNode *cur, *next, *prev;   
+    int mid = 0, count = 0;
+    
+    if (head == NULL || head->next == NULL)
+    {
+        return true;            
+    }
+ 
+    if (head->next->next == NULL)
+    {
+        return ((head->val) == (head->next->val));
+    }
+ 
+    // Find mid node
+    while (fast && fast->next)
+    {
+          fast = fast->next->next;
+          slow = slow->next;
+          mid++;
+    }
+        
+    //Reverse linked list on left side
+    cur = next = slow;
+    prev = NULL;
+    
+    while (cur)
+    {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;     
+    }
+    
+    cur = head;
+    //Compare the right and left linked list
+    while (count < mid)
+    {
+        if (cur->val != prev->val)
+        {
+            return false;
+        }      
+        cur = cur->next;
+        prev = prev->next;
+        count++;
+    }
+    
+    return true;  
+}
+
+//Sample
+bool isPalindrome2(struct ListNode* head) {
+    struct ListNode* reverse = NULL;
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+    struct ListNode *tmp;
+    while (slow && fast) {
+        if (fast->next)
+            fast = fast->next->next;
+        else
+            break;
+        
+         slow = slow->next;
+    }
+    if (fast)
+        slow = slow->next;
+    
+    while (slow) {
+        tmp = slow->next;
+        slow->next = reverse;
+        reverse = slow;
+        slow = tmp;
+    }
+   
+    while (reverse) {
+        if (head->val != reverse->val)
+            return false;
+        head = head->next;
+        reverse = reverse->next;
+    }
+    
+    return true;
+}
