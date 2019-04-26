c2ae00251b4a6edecb6e592163ca2b95c0759be7
lingyu77 <34279209+lingyu77@users.noreply.github.com>
2019-04-26 11:36:57 +0800

    961

---
+++ Java/961_N-RepeatedElementinSize2NArray.java
@@ -0,0 +1,15 @@
+// 1 ms
+class Solution {
+    public int repeatedNTimes(int[] A) {
+        Set<Integer> set = new HashSet();
+        
+        for (int i : A) {
+            if (set.contains(i))
+                return i;
+            else
+                set.add(i);
+        }
+        
+        return 0;
+    }
+}
