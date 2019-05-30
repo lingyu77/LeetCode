19da98e693cec99ceec7ac42789e9a6b5b568da6
lingyu77 <34279209+lingyu77@users.noreply.github.com>
2019-05-30 13:11:43 +0800

    1

--- Java/1_TwoSum.java
+++ Java/1_TwoSum.java
@@ -1,18 +1,18 @@
+// 2 ms
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         int index = 0;
-        for(int i : nums) {
-            map.put(i, index++);
-        } 
-        index = 0;
-        for(int i : nums) {
-            int complement = target - i;
+
+        for (int num : nums) {
+            int complement = target - num;
+            
             if (map.containsKey(complement) && map.get(complement) != index)
-                return new int[]{index, map.get(complement)};
+               return new int[]{map.get(complement), index};
             else
-                index++;
+               map.put(num, index++); 
         }
+        
         throw new IllegalArgumentException("No solution!");
     }
 }
